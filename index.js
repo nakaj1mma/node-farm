a0ce005f0de284b6237679610a2b5babe3cb51a8
idoz1 <169044973+idoz1@users.noreply.github.com>
2025-04-10 13:57:25 +0300

    Added navigation; 404 page not finished

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+//MODULES
 const http = require('http')
 const url = require('url')
 const fs = require('fs')
 
+//SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%ID%}/g, product.id)
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORAGINC%}/g, 'not-organic')
+  }
+  return output
+}
+
 const hostname = '127.0.0.1'
-const port = 3000
+const port = 8000
+
+const tempNotFound = fs.readFileSync(
+  `${__dirname}/templates/template-404.html`,
+  'utf-8'
+)
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -17,32 +40,47 @@ const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   'utf-8'
 )
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url
-  switch (pathName) {
-    //Overview page
+  const { query, pathname } = url.parse(req.url, true)
 
+  switch (pathname) {
+    //Overview page
     case '/':
     case '/overview':
       res.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8',
       })
-      res.end(tempOverview)
+      const cardsHtml = dataObj
+        .map((el) => replaceTemplate(tempCard, el))
+        .join('')
+
+      const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+
+      res.end(output)
       break
 
     //Product page
     case '/product':
+      const { id } = query
+      const product = dataObj[id]
+      if (!id || !product) {
+        res.writeHead(404, {
+          'Content-Type': 'text/html; charset=utf-8',
+        })
+        return res.end(tempNotFound)
+      }
       res.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8',
       })
-      res.end('<h1>Продукт</h1>')
+      const outputProduct = replaceTemplate(tempProduct, product)
+      res.end(outputProduct)
       break
 
     //API
-
     case '/api':
       res.writeHead(200, { 'content-type': 'application/json' })
       res.end(data)
@@ -52,9 +90,8 @@ const server = http.createServer((req, res) => {
     default:
       res.writeHead(404, {
         'Content-Type': 'text/html; charset=utf-8',
-        'my-custom': 'hello-world',
       })
-      res.end('<h1>Страница не найдена!</h1>')
+      res.end(tempNotFound)
   }
 })
 
